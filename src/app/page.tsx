52da0b056099303d2d10c6f48638cb60ea8e7f6a
Yashraj413 <yashraj41323@gmail.com>
2025-07-23 16:37:45 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-purple-200">
+      <h1 className="text-5xl font-bold text-purple-700">Next.js,Tailwind css set up 🎉</h1>
+    </main>
+  );
+}
